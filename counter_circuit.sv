10effa346cbbe538ded9371853f80889c4159641
Ukuma012 <Ukuma012@gmail.com>
2023-07-12 09:53:47 +0900

    counter

--- counter_circuit.sv
+++ counter_circuit.sv
@@ -1,3 +1,23 @@
 module couter_circuit {
     input logic CLK, RST,
+    output logic [3:0] COUNT
 };
+
+logic [3:0] n_count;
+
+always_comb begin
+    if(COUNT == 4'd9) begin
+        n_count = 4'd0;
+    end else begin
+        n_count = COUNT + 1'b1;
+    end
+end
+
+always_ff @ (posedge CLK) begin
+    if (RST) begin
+        COUNT <= 4'd0;
+    end else begin
+        COUNT <= n_count;
+    end
+end
+endmodule
