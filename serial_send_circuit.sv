620bdf6f87b3c057a7671e25b4419e72270042b5
Ukuma012 <Ukuma012@gmail.com>
2023-07-13 16:41:03 +0900

    state machine

--- serial_send_circuit.sv
+++ serial_send_circuit.sv
@@ -5,3 +5,18 @@ module serial_send_circuit (
     output logic DATA_OUT,
     output logic BUSY
 );
+
+parameter WAIT_COUNT_BASE = 868; // Tser(115.2kbps) / Tclk(0.01μs)
+localparam WAIT_LEN = $clog2(WAIT_COUNT_BASE);
+
+typedef enum{
+    STATE_IDLE,
+    STATE_SEND
+} state_type;
+
+state_type state, next_state;
+logic [9:0] data_reg, next_data_reg;
+logic [WAIT_LEN - 1: 0] wait_count, next_wait_count;
+logic [3:0] bit_count, next_bit_count;
+
+assign DATA_OUT = data_reg[0];
