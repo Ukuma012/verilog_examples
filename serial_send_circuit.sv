4c59e8d0089ce8a05eda7c1fdd02be8e6d50577c
Ukuma012 <Ukuma012@gmail.com>
2023-07-14 10:08:30 +0900

    uart

--- serial_send_circuit.sv
+++ serial_send_circuit.sv
@@ -20,3 +20,47 @@ logic [WAIT_LEN - 1: 0] wait_count, next_wait_count;
 logic [3:0] bit_count, next_bit_count;
 
 assign DATA_OUT = data_reg[0];
+
+always_comb begin
+    BUSY = 1'b0;
+    next_state = state;
+    next_wait_count = wait_count;
+    next_bit_count = bit_count;
+    next_data_reg = data_reg;
+    if (state == STATE_IDLE) begin
+        if (WE) begin
+            next_state = STATE_SEND;
+            next_data_reg = {1'b1, DATA_IN, 1'b0};
+        end
+    end else if (state == STATE_SEND) begin
+        BUSY = 1'b1;
+        if (wait_count == WAIT_COUNT_BASE - 1) begin
+            if (bit_count == 4'b9) begin
+                next_state = STATE_IDLE;
+                next_wait_count = 0;
+                next_bit_count = 4'd0; 
+            end else begin
+                next_data_reg = {1'b1, data_reg[9:1]}
+                next_wait_count = 0;
+                next_bit_count = bit_count + 1'b1;
+            end
+        end else begin
+            next_wait_count = wait_count + 1'b1;
+        end
+    end
+end
+
+always always_ff @( CLK ) begin
+    if (RST) begin
+        state <= STATE_IDLE;
+        wait_count <= 0;
+        bit_count <= 4'd0;
+        data_reg <= 10'h3ff;
+    end else begin
+        state <= next_state;
+        wait_count <= next_wait_count;
+        bit_count <= next_bit_count;
+        data_reg <= next_data_reg;
+    end
+end
+endmodule
